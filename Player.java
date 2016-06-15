aa812c93d9461ab0bcb05ae065b71412cc2316b8
Damani <damani.walder@live.com>
2016-06-15 00:58:13 -0400

    converted pDominos to ArrayList called hand

--- Player.java
+++ Player.java
@@ -1,16 +1,15 @@
 package GameFiles;
 
-//TODO finish deadDouble method - after creating numCount in GameModel
-
-//TODO - finish play method
+import java.util.ArrayList;
+import java.util.List;
 	
 //TODO - finish pose method
 
 //TODO (if simulating game) -> sort domino in list based on sum of each domino 
 
-	public class Player {
-	
-	Domino [] pDominos;
+public class Player {
+	//list of dominos in players hand
+	List<Domino> hand;
 	
 	//keeps a track of the number of doubles a player has in their hand
 	int numDoubles = 0;
@@ -20,42 +19,59 @@ package GameFiles;
 	
 	public Player(){
 		this.numDominos = 0;
+		this.hand = new ArrayList<Domino>();
 	}
 	
 	public void populateHand(Domino d){
-		this.pDominos[numDominos] = d;
-		if (pDominos[numDominos].isDouble()){
+		hand.add(d);
+		if (d.isDouble()){
 			numDoubles ++;
 		}
+		//will be used for debugging purposes
 		numDominos++;
 	}
 	
 	//method which determines if a player has double 6
-	//method will be ran at start of game 
-	public boolean haveDoubleSix()
+	//method will be ran at start of game to determine which player
+	//plays first
+	/*public boolean haveDoubleSix()
 	{
 		if(numDoubles>0){
-			for (int i=0;i<7;i++){
-				if(pDominos[i].isDouble()){
-					if(pDominos[i].getSide1() == 6){
+			for (int i=0;i<hand.size();i++){
+				if(hand.get(i).isDouble()){
+					if(hand.get(i).getSide1() == 6){
 						return true;	
 					}
 				}
 			}
 		}
 		return false;
-	}	
+	}*/	
 	
 	public int sumDominos(){
 		int sum=0;
-		for(int i=0; i<=numDominos;i++){
-			 sum += pDominos[numDominos].getSum();
+		for(int i=0; i<=hand.size();i++){
+			 sum += hand.get(i).getSum();
 		}
 		return sum;
 	}
 	
-	//function which determines whether a player has a dead double in their hand
-	//returns 1 if they do, otherwise 0 
+	//method responsible for posing doubleSix
+	void pose(GameBoard gb){
+		hand.remove(new Domino(6,6));
+		gb.setEnd1(6);
+		gb.setEnd2(6);
+		gb.changeNumDomPlayed(1);
+	}
+		
+	//the play of the player
+	void play(GameBoard gb){
+		
+	}
+	
+	
+	/*
+	//function which determines whether a player has a dead double in their hand 
 	public boolean isDeadDoubleInHand()
 	{
 		if (numDoubles > 0){
@@ -81,15 +97,5 @@ package GameFiles;
 		}
 		return num;
 	}
-	
-	//method responsible for posing doubleSix
-	void pose(){
-		//cycle thru dominos and find the double six
-		//play the double six
-	}
-	
-	//the play of the player
-	void play(GameBoard gb){
-		
-	}
+	*/
 }
