34e5ec54ab88add7a972ff15bec358dbaf11b9be
Damani Walder <dgwalder@users.noreply.github.com>
2017-04-23 23:39:03 -0400

    updated Gameboard class
    modified isMoveAvailable method and added playDomino method. Also added a graveyard which stores all the dominos which have been played

--- GameBoard.java
+++ GameBoard.java
@@ -1,52 +1,87 @@
-package GameFiles;
+package GameCode;
 
-public class GameBoard {
+import java.util.ArrayList;
+import java.util.List;
+
+final class GameBoard {
 	//stores the number of dominos played so far 
-	//initialized to 0
 	private int numDominosPlayed = 0;
+	private List<Domino> graveyard = new ArrayList<>();
 
-	//aEnd1 and aEnd2 stores the values of the available options on the board
+	//stores the values at the available ends of the board
 	private int aEnd1, aEnd2;
-	
+		
 	//keeps track of how much of each number has been played so far 
 	int [] numSum;
-	
-	public GameBoard (){
-		this.numDominosPlayed = 0;
+		
+	GameBoard (){
+		numDominosPlayed = 0;
 		numSum = new int [6];
 	}
-	
-	public void setEnd1(int a){
-		this.aEnd1 = a;
-	}
-	
-	public void setEnd2(int a){
-		this.aEnd2 = a;
-	}
-	
-	public int getEnd1(){
-		return this.aEnd1;
-	}
-	
-	public int getEnd2(){
-		return this.aEnd2;
-	}
-	
-	public void setNumDominosPlayed(int a){
-		this.numDominosPlayed+= a;
-	}
-		
-	//function which determines if a game is blocked
-	//a game is blocked when the 2 ends of the board have equal values 
-	//and 7 dominos (including the double) or 6 (without the double) have already been playerd
+			
+	/**
+	 * determines if GameBoard is blocked
+	 * @return
+	 */
 	public boolean isBlocked()
 	{
-		if (aEnd1 == aEnd2)
-		{
-			if (numSum[aEnd1] >= 6){
-				return true;
-			}
+		return ((aEnd1 == aEnd2) && (numSum[aEnd1] >= 6));
+	}
+	
+	/**
+	 * checks if a move is available for given domino
+	 * @return
+	 */
+	boolean isMoveAvailable(Domino d){
+		boolean moveAvailable = false;
+		if (d.getSide1() == getEnd1()){
+			moveAvailable = true;
+		}else if (d.getSide2() == getEnd1()){
+			moveAvailable = true;
+		}else if (d.getSide1() == getEnd2()){
+			moveAvailable = true;
+		}else if (d.getSide2() == getEnd2()){
+			moveAvailable = true;
 		}
-		return false;
+		return moveAvailable;
+	}
+
+	void pose(Domino d){
+		setEnd1(d.getSide1());
+		setEnd2(d.getSide2());
+		graveyard.add(d);
+	}
+
+	void playDomino(Domino d) {
+		if (d.getSide1() == getEnd1()){
+			setEnd1(d.getSide2());
+		}else if (d.getSide2() == getEnd1()){
+			setEnd1(d.getSide1());
+		}else if (d.getSide1() == getEnd2()){
+			setEnd2(d.getSide2());
+		}else {
+			setEnd2(d.getSide1());
+		}
+		graveyard.add(d);
+		numDominosPlayed++;
+		
+		//Todo update the num sum variable when a card is played
+	}
+	
+	/******* Getters and Setters *****/
+	private void setEnd1(int a){
+		aEnd1 = a;
+	}
+		
+	private void setEnd2(int a){
+		aEnd2 = a;
+	}
+
+	int getEnd1(){
+		return aEnd1;
+	}
+		
+	int getEnd2(){
+		return this.aEnd2;
 	}
 }
